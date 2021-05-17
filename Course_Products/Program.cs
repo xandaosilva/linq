8884ebedcfc6736fe3beb4bde0292da2936911b6
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-05-17 11:16:39 -0300

    Alternative linq expressions

--- Course_Products/Program.cs
+++ Course_Products/Program.cs
@@ -41,19 +41,24 @@ namespace Course_Products
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            var r1 = from p in products where p.Category.Tier == 1 && p.Price < 900.0 select p;
             Print("TIER 1 AND PRICE < 900.0 :", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 = from p in products where p.Category.Name == "Tools" select p.Name;
             Print("NAMES OF PRODUCTS FROM TOOLS :", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price , CategoryName = p.Category.Name });
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price , CategoryName = p.Category.Name });
+            var r3 = from p in products where p.Name[0] == 'C' select new { p.Name, p.Price, CategoryName = p.Category.Name };
             Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT :", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 = from p in products where p.Category.Tier == 1 orderby p.Name orderby p.Price select p;
             Print("TIER 1 ORDER BY PRICE THEN BY NAME :", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 = (from p in r4 select p).Skip(2).Take(4);
             Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4 :", r5);
 
             var r6 = products.FirstOrDefault();
@@ -87,7 +92,8 @@ namespace Course_Products
             Console.WriteLine("CATEGORY 1 AGGREGATE SUM : " + r15.ToString("F2", CI));
             Console.WriteLine();
 
-            var r16 = products.GroupBy(p => p.Category);
+            //var r16 = products.GroupBy(p => p.Category);
+            var r16 = from p in products group p by p.Category;
             foreach (IGrouping<Category, Product> group in r16)
             {
                 Console.WriteLine("CATEGORY " + group.Key.Name + " : ");

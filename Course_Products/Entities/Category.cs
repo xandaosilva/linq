dd9f574a5921ef3ade30bc5f73c179909a579dce
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-05-17 09:12:42 -0300

    Adicionando classes

--- Course_Products/Entities/Category.cs
+++ Course_Products/Entities/Category.cs
@@ -6,5 +6,10 @@ namespace Course_Products.Entities
 {
     class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+
     }
 }
--- Course_Products/Entities/Product.cs
+++ Course_Products/Entities/Product.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace Course_Products.Entities
 {
     class Product
     {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Price.ToString("F2",CI) + ", " + Category.Name + ", " + Category.Tier;
+        }
     }
 }
